e39eb293d29a805e65462ee22e4a7b4f47bce880
Blanky_9923 <blankita77@gmail.com>
2024-03-30 02:44:39 +0100

    Añadir ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+const miPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promesa resuelta')
+    }, 3000)
+})
+
+miPromesa.then((resultado) => {
+    console.log(resultado)
+})
