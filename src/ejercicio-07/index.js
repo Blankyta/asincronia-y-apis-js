e0fd18e3aa621489feaf5605971ff0ee99af7955
Blanky_9923 <blankita77@gmail.com>
2024-04-20 01:48:57 +0200

    Añadir ejercicio-07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+axios
+.getAdapter('https://jsonplaceholder.typicode.com/post/1')
+.then((response) => {
+    console.log(response.data)
+})
+.catch((error) => {
+    console.error(error)
+})
